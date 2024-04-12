a34ed2bd8546493b6e5b2fe5d2d2a565e405967e
LeeYeongin <youngin5757@gmail.com>
2024-04-12 17:55:24 +0900

    insert member table & member table, board table 조회 함수 구현

--- src/edu/pnu/QueryExecuteUpdate.java
+++ src/edu/pnu/QueryExecuteUpdate.java
@@ -3,6 +3,7 @@ package edu.pnu;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Random;
@@ -12,47 +13,91 @@ import java.util.Scanner;
 public class QueryExecuteUpdate {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num, year;
+		int num, year, select = 0;
 		String s1, s2;
 
 		// No suitable driver found for jdbc:h2 오류
 		// package Build path -> library -> class path -> program(x86) -> H2 -> bin ->
 		// jar파일
 		try (Connection con = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/.h2/sqlprg", "sa", "abcd")) {
-			if (!createTable(con))
-				return;
-			if (!InsertData(con))
-				return;
+			
+			while(true) {
+				System.out.print("(0)종료 (1) 테이블 생성 (2) 데이터 입력 (3) Board 테이블 데이터 삭제\n"
+						+ "(4) Member 테이블 데이터 삭제 (5) Board 테이블 데이터 수정 (6) Member 테이블 데이터 수정\n"
+						+ "(7) Member 테이블 조회 (8) Board 테이블 조회: ");
+				select = sc.nextInt();
+				switch (select) {
+					case 0:
+						System.out.println("Done");
+						return;
+					case 1:
+						if (!createTable(con))
+							return;
+						break;
+					case 2:
+						if (!InsertData(con))
+							return;
+						break;
+					case 3:
+						System.out.print("Board에서 삭제할 num을 입력하세요: ");
+						num = sc.nextInt();
+						if (!DeleteBoardData(con, num))
+							return;
+						break;
+					case 4:
+						System.out.print("Member에서 삭제할 id를 입력하세요: ");
+						num = sc.nextInt();
+						if (!DeleteMemData(con, num))
+							return;
+						break;
+					case 5:
+						System.out.print("Board에서 수정할 num을 입력하세요: ");
+						num = sc.nextInt();
+						sc.nextLine();
+						System.out.print("새로운 title: ");
+						s1 = sc.nextLine();
+						System.out.print("새로운 content: ");
+						s2 = sc.nextLine();
+						System.out.println(s2);
+						if (!UpdateBoardData(con, num, s1, s2))
+							return;
+						break;
+					case 6:
+						String username = null, password = null;
+						
+						System.out.print("Member에서 수정할 id를 입력하세요: ");
+						num = sc.nextInt();
+						sc.nextLine();
+						
+						System.out.print("새로운 username를 입력하세요(수정할 값이 아니라면 enter key를 누르세요): ");
+						username = sc.nextLine();
+						if(username.isEmpty())
+							username = null;
 
-			System.out.print("Board에서 삭제할 num을 입력하세요: ");
-			num = sc.nextInt();
-			if (!DeleteBoardData(con, num))
-				return;
-
-			System.out.print("Member에서 삭제할 id를 입력하세요: ");
-			num = sc.nextInt();
-			if (!DeleteMemData(con, num))
-				return;
-
-			System.out.print("Board에서 수정할 num을 입력하세요: ");
-			num = sc.nextInt();
-			System.out.print("새로운 title: ");
-			s1 = sc.next();
-			System.out.print("새로운 content: ");
-			s2 = sc.next();
-			System.out.println(s2);
-			if (!UpdateBoardData(con, num, s1, s2))
-				return;
-
-//			String username = null, password = null;
-//			System.out.print("Member에서 수정할 id를 입력하세요: ");
-//			num = sc.nextInt();
-//			System.out.print("새로운 username, password, birthyear를 입력하세요: ");
-
+						System.out.print("새로운 password를 입력하세요(수정할 값이 아니라면 enter key를 누르세요): ");
+						password = sc.nextLine();
+						if(password.isEmpty())
+							password = null;
+						
+						System.out.print("새로운 birthyear를 입력하세요(수정할 값이 아니라면 0을 입력하세요): ");
+						year = sc.nextInt();
+						
+						if(!UpdateMemData(con, num, username, password, year))
+							return;
+						break;
+					case 7:
+						if(!showUserList(con))
+							return;
+						break;
+					case 8:
+						if(!showBoardList(con))
+							return;
+						break;
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("Done");
 	}
 
 	public static boolean createTable(Connection con) {
@@ -80,6 +125,7 @@ public class QueryExecuteUpdate {
 				e.printStackTrace();
 			}
 		}
+		System.out.println("테이블이 생성되었습니다.");
 		return true;
 	}
 
@@ -175,31 +221,97 @@ public class QueryExecuteUpdate {
 		return true;
 	}
 
-	public static boolean UpdateMemData(Connection con, int num, String user, String pwd, int year) {
+	public static boolean UpdateMemData(Connection con, int id, String user, String pwd, int year) {
 		try {
-			PreparedStatement psmt = con.prepareStatement(
-							"UPDATE Member " 
-							+ "SET username = CASE WHEN ? IS NOT NULL THEN ? ELSE username END, "
-							+ "password = CASE WHEN ? IS NOT NULL THEN ? ELSE password END, " 
-							+ "birthyear = CASE WHEN ? != 0 THEN ? ELSE password END "
-							+ "WHERE id = ?");
-			psmt.setString(1, user);
-			psmt.setString(2, user);
-			psmt.setString(3, pwd);
-			psmt.setString(4, pwd);
-			psmt.setInt(5, year);
-			psmt.setInt(6, year);
-			psmt.setInt(7, num);
-
-			int result = psmt.executeUpdate();
-			if (result == 0)
-				System.out.println("해당 데이터가 없습니다.");
-			else
-				System.out.println("Board 데이블에서 " + num + "데이터 " + result + "개가 수정되었습니다.");
+			// 코드 1
+			String str = "";
+			if(user != null) {
+				str += "username='" + user + "' ";
+			}
+			if(pwd != null) {
+				if(!str.isEmpty()) str += ",";
+				str += "password='" + pwd + "' ";
+			}
+			if(year != 0) {
+				if(!str.isEmpty()) str += ",";
+				str += "birthyear='" + year + "' ";
+			}
+			
+			String sql = "Update member set " + str + "where id=" + id;
+			Statement st = con.createStatement();
+			st.executeUpdate(sql);
+			
+			
+			// 코드 2?
+//			PreparedStatement psmt = con.prepareStatement(
+//							"UPDATE Member " 
+//							+ "SET username = CASE WHEN ? IS NOT NULL THEN ? ELSE username END, "
+//							+ "password = CASE WHEN ? IS NOT NULL THEN ? ELSE password END, " 
+//							+ "birthyear = CASE WHEN ? != 0 THEN ? ELSE birthyear END "
+//							+ "WHERE id = ?");
+//			psmt.setString(1, user);
+//			psmt.setString(2, user);
+//			psmt.setString(3, pwd);
+//			psmt.setString(4, pwd);
+//			psmt.setInt(5, year);
+//			psmt.setInt(6, year);
+//			psmt.setInt(7, id);
+//
+//			int result = psmt.executeUpdate();
+//			if (result == 0)
+//				System.out.println("해당 데이터가 없습니다.");
+//			else
+//				System.out.println("Member 데이블에서 " + id + "데이터 " + result + "개가 수정되었습니다.");
+			
+			System.out.println("Member 테이블이 수정되었습니다.");
 			System.out.println();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return true;
+	}
+	
+	public static boolean showUserList(Connection con) {
+		try {
+			String sql = "Select * from Member";
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			
+			while(rs.next()) {
+				System.out.print(rs.getString("username")+","); 
+				System.out.print(rs.getString("password")+",");
+				System.out.print(rs.getInt("birthyear")+",");
+				System.out.print(rs.getString("regidate")+"\n");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return true;
+	}
+	
+	public static boolean showBoardList(Connection con) {
+		try {
+			PreparedStatement psmt = con.prepareStatement("Select * from Board limit ?,5");
+
+			for(int i=0; i<=9; i++) {
+				psmt.setInt(1, i*5);				
+				ResultSet rs = psmt.executeQuery();
+				
+				while(rs.next()) {
+					System.out.print(rs.getString("num")+","); 
+					System.out.print(rs.getString("title")+",");
+					System.out.print(rs.getString("content")+",");
+					System.out.print(rs.getInt("id")+",");
+					System.out.print(rs.getString("postdate")+",");
+					System.out.print(rs.getInt("visitcount")+"\n");
+				}
+				System.out.println("------------------------------------------------------");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		return true;
 	}
 }
